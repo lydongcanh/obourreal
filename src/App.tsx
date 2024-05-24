9aa12794e2a98e5e39e2d6f494f44f83d3ef38b4
Ted Ly <ted.ly@ansarada.com>
2024-05-24 20:21:48 +0700

    Install Mantine.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,23 @@
+import { AreaChart } from "@mantine/charts";
+import { data } from "./data.ts";
+
 function App() {
-  return "Hello, Ted & Olivia"
+  return (
+    <div style={{ padding: 24 }}>
+      <AreaChart
+        h={300}
+        data={data}
+        dataKey="date"
+        series={[
+          { name: "Apples", color: "indigo.6" },
+          { name: "Oranges", color: "blue.6" },
+          { name: "Tomatoes", color: "teal.6" },
+        ]}
+        curveType="linear"
+        style={{ maxWidth: 500 }}
+      />
+    </div>
+  );
 }
 
-export default App
+export default App;
