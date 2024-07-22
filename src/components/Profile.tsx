f9a95826e499dad5bb6eaeeca68fac26c8de0d6d
Ted Ly <lydongcanh@gmail.com>
2024-07-22 23:30:30 +0700

    Add login again button.

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -11,19 +11,20 @@ import {
 } from "@mantine/core";
 import { useAuth0 } from "@auth0/auth0-react";
 import { IconUser, IconId, IconKey } from "@tabler/icons-react";
-import { jwtDecode } from 'jwt-decode';
+import { jwtDecode } from "jwt-decode";
 
 type DataObject = Record<string, unknown>;
 
 const Profile: React.FC = () => {
-  const { user, isLoading, getAccessTokenSilently } = useAuth0();
+  const { user, isLoading, getAccessTokenSilently, loginWithRedirect } =
+    useAuth0();
   const [accessToken, setAccessToken] = useState<string | null>(null);
   const [tokenInfo, setTokenInfo] = useState<DataObject | null>(null);
 
   const handleGetAccessToken = async () => {
     try {
       const token = await getAccessTokenSilently();
-      console.log("Get access token successfully " + token)
+      console.log("Get access token successfully " + token);
       setAccessToken(token);
 
       // Decode the JWT token
@@ -34,6 +35,14 @@ const Profile: React.FC = () => {
       console.error("Error getting access token:", error);
       setTokenInfo(null);
     }
+  };
+
+  const handleLoginAgain = async () => {
+    loginWithRedirect({
+      authorizationParams: {
+        randomValue: crypto.randomUUID(),
+      },
+    });
   };
 
   const userProfileFields = [
@@ -152,6 +161,18 @@ const Profile: React.FC = () => {
               ) as DataObject)
             : null
         )}
+
+        <Button
+          onClick={handleLoginAgain}
+          style={{
+            background: "linear-gradient(45deg, #4a90e2 0%, #63b3ed 100%)",
+            border: "none",
+            boxShadow:
+              "0 4px 6px rgba(50, 50, 93, 0.11), 0 1px 3px rgba(0, 0, 0, 0.08)",
+          }}
+        >
+          Login Again
+        </Button>
 
         <Button
           onClick={handleGetAccessToken}
